e89cbeb79bc9fc402e91674f3551d50913ab97f9
alexisospina1993 <64335871+alexisospina1993@users.noreply.github.com>
2023-05-12 12:06:05 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My parents new helthy diner');
 
-streamlit.header('Breakfast Menu');
-streamlit.text('omega 3 & blueberry Oatmeal');
-streamlit.text('Kale, spinach & Rocjet smoothie');
-streamlit.text('hard-Boild Free-Range Egg');
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
